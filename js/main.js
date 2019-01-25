ecbd1ce5c2f9a3134c025da1edfbc3d7c8c07130
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-01-24 18:09:10 -0800

    Finished main assignment, I guess?

--- js/main.js
+++ js/main.js
@@ -1,40 +1,57 @@
+// getPositiveInteger prompts the user for a positive integer.
+// It returns the positive integer entered.
+function getPositiveInteger()
+{
+    let isInteger = false, outputNumber = 0;
+    while (!isInteger)
+    {
+        outputNumber = Number(prompt('Please enter a positive integer:', 25));
+        isInteger = (Number.isSafeInteger(outputNumber) && (outputNumber>0));
+        console.log(outputNumber, isInteger);
+    }
+    return outputNumber;
+}
+
+// calculateFizzBuzz returns an array of strings representing the outcome of the FizzBuzz
+function calculateFizzBuzz(upperLimit)
+{
+    let outputArray = [];
+
+    for (let count = 1; count < maxNumber+1; count++)
+    {
+        // note to self: only one statement at max executes
+        if ((count % 15) == 0)
+        {
+            outputArray.push('FizzBuzz');
+        }
+        else if ((count % 5) == 0)
+        {
+            outputArray.push('Buzz');
+        }
+        else if ((count % 3) == 0)
+        {
+            outputArray.push('Fizz');
+        }
+        else
+        {
+            outputArray.push(count.toString());
+        }
+        
+    }
+    return outputArray;
+}
+
 /* Code for WATS 3020 FizzBuzz Assignment */
 
-let isInteger,
-    maxNumber,
+let maxNumber,
     fbResults,
     fbText;
 
-
-// TODO: Initialize a variable `isInteger` to use as a control value for the
-// `while` loop. Set the initial value to `false`.
-
-// TODO: Create a `while` loop so user will be prompted to enter a number until
-// they enter a good number. This loop should be controlled by a conditional
-// expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
-// becomes `true`, the `while` loop should stop looping.
-
-// TODO: Inside `while` loop prompt the user for the `maxNumber` value.
-
-// TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
-// is a suitable integer. (It should be an integer greater than zero.)
-
-// TODO: If the value of `maxNumber` is suitable, then change the value of
-// `isInteger` so the `while` loop stops looping.
-
-// TODO: Initialize the `fbResults` variable to an empty Array
-
-// TODO: Create a `for` loop that will execute the `maxNumber` of times.
-
-// TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
-// require the use of several conditional statements that use the `%` operator.
-// Store the results of this logic in an array called `fbResults`.
-
-// TODO: In preparation for creating the output text for your FizzBuzz results,
-// initialize the `fbText` variable to an empty string.
-
-// TODO: Use a `for ... of` loop to concatenate the values from `fbResults`
-// into the variable `fbText`.
+// Get the maximum number for the FizzBuzz
+maxNumber = getPositiveInteger();
+fbResults = calculateFizzBuzz(maxNumber);
+// the assignment called for a for loop to do this. Join with a newline character seemed to achieve the same result.
+fbText = fbResults.join('\n');
 
 
 ///////////////////////////////////////////////////////////////////////
